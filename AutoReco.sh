df20d68afb989a8a632fdc78a47562cf3e126e35
E-Nico <nicomagny@outlook.fr>
2025-02-28 22:16:26 +0100

    update script

--- AutoReco.sh
+++ AutoReco.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 
 RED='\e[31m'
 GREEN='\e[32m'
@@ -19,17 +19,35 @@ else
 	export TARGET=$var
 fi
 
+
+
+if ping -c 3 "$TARGET" | grep -q "Destination Host Unreachable"; then
+	echo "${RED}L'hôte est deco, vérifie que la machine est lancée et que tu as bien lancé ton VPN ${NC}"
+else
+	echo "${GREEN}MACHINE UP${NC}"
+fi
+
 echo -e "${YELLOW}[/\] START SCANNING${NC}"
 
 echo -e "${MAGENTA}[*] SCAN NMAP ${NC}"
-result_nmap=nmap -sCV $TARGET
+result_nmap=$(nmap -sCV $TARGET)
+echo -e "$result_nmap"
 
 #if port 80
 echo -e "${MAGENTA}[*] SUBDOMAINS SCANS${NC}"
-#result_gobuster=gobuster dir -u http://$TARGET/ -w /usr/share/dirb/wordlists/common.txt -x php,txt,html -b 301 --exclude-length 6609 -k
-resultat_dirb=dirb http://$TARGET/
-resultat_ferox=feroxbuster -u http://$TARGET
+#resul_gobuster=gobuster dir -u http://$TARGET/ -w /usr/share/dirb/wordlists/common.txt -x php,txt,html -b 301 --exclude-length 6609 -k
+echo -e "${YELLOW}[-] LANCEMENT DIRB${NC}"
+dirb http://$TARGET/
+echo -e "${YELLOW}[-] LANCEMENT FEROXBUSTER${NC}"
+feroxbuster -u http://$TARGET -C 503
 echo -e "${MAGENTA}[*] VHOST SCANS${NC}"
-resultat_vhost=fuff -c -w /usr/share/dirbuster/wordlists/directory-list-2.3-medium.txt -u http://$TARGET  -H "Host: FUZZ.${DOMAIN_NAME}" --fc 301
-echo -e "${MAGENTA}[*] WEB SCANNING"
-resultat_nikto=nikto -host $TARGET
+echo -e "${YELLOW}[-] LANCEMENT FFUF${NC}"
+ffuf -c -w /usr/share/dirbuster/wordlists/directory-list-2.3-medium.txt -u http://$TARGET  -H "Host: FUZZ.$TARGETi" --fc 301,302
+echo -e "${MAGENTA}[*] WEB SCANNING${NC}"
+echo -e "${YELLOW}[-] LANCEMENT NIKTO${NC}"
+nikto -host $TARGET
+echo -e "${YELLOW}[-] LANCEMENT WHATWEB (CMS)${NC}"
+whatweb $TARGET
+echo -e "${MAGENTA}[*] DEEP ANALYSE${NC}"
+nmap -sU $TARGET
+echo -e " ~ FIN DE LA PHASE DE RECONNAISSANCE ! ~"
